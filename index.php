38d6ecf07ece7526fa9e0dbe5773a604fd0387e8
tonybennett <anthony@exhibit-x.com>
2019-08-22 16:25:40 -0400

    test heroku deploy

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+
+/* 
+ * Redirect to a different page in the current directory that was requested 
+ */
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+header("Location: http://$host$uri/dtpr_prototype/index.html");
+exit;
+?>
