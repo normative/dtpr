057ad7dd91e7562b4da4f8ab010bd5f561f643f9
tonybennett <anthony@exhibit-x.com>
2019-08-22 16:26:37 -0400

    test heroku deploy

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 $host  = $_SERVER['HTTP_HOST'];
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 
-header("Location: http://$host$uri/dtpr_prototype/index.html");
+header("Location: https://$host$uri/dtpr_prototype/index.html");
 exit;
 ?>
